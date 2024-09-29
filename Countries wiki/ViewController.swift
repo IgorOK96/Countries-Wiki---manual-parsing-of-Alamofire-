003a5944ddc8f28027eb60e7786561efa19ff1d6
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-09-29 03:41:20 +0200

    Initial Commit

---
+++ Countries wiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries wiki
+//
+//  Created by user246073 on 9/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
